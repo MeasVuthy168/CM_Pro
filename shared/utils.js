3b8daee5062076248880a28bc80c06ea2c57597e
MeasVuthy168 <measvuthy21@gmail.com>
2026-05-23 13:17:58 +0700

    Update utils.js

--- shared/utils.js
+++ shared/utils.js
@@ -1,1 +1,12 @@
+window.CM_UTILS = {
 
+    async loadComponent(id, file){
+
+        const response = await fetch(file);
+
+        const html = await response.text();
+
+        document.getElementById(id).innerHTML = html;
+    }
+
+};
