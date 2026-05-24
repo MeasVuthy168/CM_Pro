0ce96fdf45e5f18a79a74c7afb3aabcefcf4135e
MeasVuthy168 <measvuthy21@gmail.com>
2026-05-24 10:55:41 +0700

    Update api.js

--- config/api.js
+++ config/api.js
@@ -1,13 +1,32 @@
-
 window.API = {
 
     BASE_URL:
         "https://cm-backend-new.onrender.com",
 
+    // =========================
+    // GET TOKEN
+    // =========================
+
+    getToken(){
+
+        return (
+
+            localStorage.getItem("token") ||
+
+            sessionStorage.getItem("token")
+
+        );
+
+    },
+
+    // =========================
+    // POST
+    // =========================
+
     async post(endpoint, data){
 
         const token =
-            sessionStorage.getItem("token");
+            this.getToken();
 
         const response = await fetch(
 
@@ -17,25 +36,33 @@ window.API = {
                 method:"POST",
 
                 headers:{
+
                     "Content-Type":
                         "application/json",
 
                     Authorization:
                         `Bearer ${token}`
+
                 },
 
                 body:
                     JSON.stringify(data)
+
             }
         );
 
         return response.json();
+
     },
+
+    // =========================
+    // GET
+    // =========================
 
     async get(endpoint){
 
         const token =
-            sessionStorage.getItem("token");
+            this.getToken();
 
         const response = await fetch(
 
@@ -43,13 +70,16 @@ window.API = {
 
             {
                 headers:{
+
                     Authorization:
                         `Bearer ${token}`
+
                 }
             }
         );
 
         return response.json();
+
     }
 
 };
