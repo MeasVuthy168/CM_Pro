b9d0f56463b4e4b9dfcfd20dfcb269bb9684e9b3
MeasVuthy168 <measvuthy21@gmail.com>
2026-05-25 23:53:17 +0700

    Create session.js

---
+++ config/session.js
@@ -0,0 +1,100 @@
+<script>
+
+    // =========================
+    // AUTO LOGOUT TIMER
+    // =========================
+
+    const SESSION_TIMEOUT =
+        1 * 60 * 1000;
+
+    // 15 minutes
+
+    let inactivityTimer;
+
+    // =========================
+    // RESET TIMER
+    // =========================
+
+    function resetSessionTimer(){
+
+        clearTimeout(
+            inactivityTimer
+        );
+
+        inactivityTimer = setTimeout(
+
+            autoLogout,
+
+            SESSION_TIMEOUT
+
+        );
+
+    }
+
+    // =========================
+    // AUTO LOGOUT
+    // =========================
+
+    function autoLogout(){
+
+        alert(
+            "Session expired due to inactivity"
+        );
+
+        // REMOVE TOKEN ONLY
+
+        localStorage.removeItem(
+            "token"
+        );
+
+        localStorage.removeItem(
+            "loggedInUser"
+        );
+
+        sessionStorage.clear();
+
+        // KEEP remember_login
+
+        window.location.replace(
+            "/CM_Pro/login.html"
+        );
+
+    }
+
+    // =========================
+    // TRACK USER ACTIVITY
+    // =========================
+
+    [
+
+        "click",
+
+        "touchstart",
+
+        "mousemove",
+
+        "keydown",
+
+        "scroll"
+
+    ].forEach(eventType => {
+
+        document.addEventListener(
+
+            eventType,
+
+            resetSessionTimer,
+
+            true
+
+        );
+
+    });
+
+    // =========================
+    // START TIMER
+    // =========================
+
+    resetSessionTimer();
+
+</script>
