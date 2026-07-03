0b763871a5cd57da70bd21166b624d96d3b8510a
MeasVuthy168 <measvuthy21@gmail.com>
2026-07-03 10:45:45 +0700

    Add initial turnover.js for UI interactions

---
+++ assets/js/turnover.js
@@ -0,0 +1,28 @@
+// ========================================
+// Turnover Page
+// Phase 1 UI only
+// ========================================
+
+document
+.getElementById("btnSearch")
+.onclick=function(){
+
+alert("Phase 2 : Search API");
+
+};
+
+document
+.getElementById("btnExcel")
+.onclick=function(){
+
+alert("Phase 3 : Export Excel");
+
+};
+
+document
+.getElementById("btnPDF")
+.onclick=function(){
+
+alert("Phase 3 : Export PDF");
+
+};
