3fb370904a02384807b23b8dd3dd43713314acc1
MeasVuthy168 <measvuthy21@gmail.com>
2026-07-11 18:25:09 +0700

    Update service worker version to v47

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ console.log("🔥 SERVICE WORKER LOADED");
    CM_Pro – Production Service Worker
 ========================================================= */
 
-const SW_VERSION = "v45";
+const SW_VERSION = "v47";
 
 const CACHE_NAME = `cm-pro-cache-${SW_VERSION}`;
 
