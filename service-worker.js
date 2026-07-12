78bd4ccb3a7945f666903b6b07f7e08313d6e4de
MeasVuthy168 <measvuthy21@gmail.com>
2026-07-12 09:05:56 +0700

    Update service worker version to v59

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ console.log("🔥 SERVICE WORKER LOADED");
    CM_Pro – Production Service Worker
 ========================================================= */
 
-const SW_VERSION = "v58";
+const SW_VERSION = "v59";
 
 const CACHE_NAME = `cm-pro-cache-${SW_VERSION}`;
 
