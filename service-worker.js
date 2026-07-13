1a849bb23b6c33b7b04131c13fe64549e8d38320
MeasVuthy168 <measvuthy21@gmail.com>
2026-07-13 20:19:31 +0700

    Update service worker version to v88

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ console.log("🔥 SERVICE WORKER LOADED");
    CM_Pro – Production Service Worker
 ========================================================= */
 
-const SW_VERSION = "v87";
+const SW_VERSION = "v88";
 
 const CACHE_NAME = `cm-pro-cache-${SW_VERSION}`;
 
