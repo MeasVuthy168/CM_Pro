4f6a008a833fb928b67402c4da1111827fd8c15d
MeasVuthy168 <measvuthy21@gmail.com>
2026-07-18 02:02:03 +0700

    Update service worker version to v134

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ console.log("🔥 SERVICE WORKER LOADED");
    CM_Pro – Production Service Worker
 ========================================================= */
 
-const SW_VERSION = "v133";
+const SW_VERSION = "v134";
 
 const CACHE_NAME = `cm-pro-cache-${SW_VERSION}`;
 
