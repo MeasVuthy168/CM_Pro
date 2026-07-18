11ddc786574b2904f7b4508b9dd066920f797754
MeasVuthy168 <measvuthy21@gmail.com>
2026-07-18 07:59:14 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ console.log("🔥 SERVICE WORKER LOADED");
    CM_Pro – Production Service Worker
 ========================================================= */
 
-const SW_VERSION = "v143";
+const SW_VERSION = "v144";
 
 const CACHE_NAME = `cm-pro-cache-${SW_VERSION}`;
 
