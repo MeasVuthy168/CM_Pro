7c03b3b510c5310d2a41e1205eaa3849f1392753
MeasVuthy168 <measvuthy21@gmail.com>
2026-08-08 19:11:49 +0700

    Update service worker version to v213

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ console.log("🔥 SERVICE WORKER LOADED");
    CM_Pro – Production Service Worker
 ========================================================= */
 
-const SW_VERSION = "v212";
+const SW_VERSION = "v213";
 
 const CACHE_NAME = `cm-pro-cache-${SW_VERSION}`;
 
